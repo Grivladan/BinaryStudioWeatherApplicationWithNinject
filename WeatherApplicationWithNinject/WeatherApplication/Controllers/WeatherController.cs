37a3a74e756cadd85fee1b5185d40b52b82cc02f
Vladyslav Hryhorenko <grigorenkovlad1993@gmail.com>
2016-07-10 21:57:19 +0300

    Initial commit

---
+++ WeatherApplicationWithNinject/WeatherApplication/Controllers/WeatherController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApplication.Services;
+using Ninject;
+
+namespace WeatherApplication.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private IWeatherService service;
+        // GET: Weather
+        public WeatherController(IWeatherService _service)
+        {
+            service = _service;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetWeather(string city,int term)
+        {
+            var result = service.GetWeatherForecast(city);
+            ViewBag.days = term;
+            return View(result);
+        }
+
+    }
+}
---
+++ WeatherApplicationWithNinject/WeatherApplication/Services/WeatherService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Net;
+using WeatherApplication.Models;
+using Newtonsoft.Json;
+
+namespace WeatherApplication.Services
+{
+    public interface IWeatherService
+    {
+        WeatherData.RootObject GetWeatherForecast(string city);
+    }
+
+    public class WeatherService: IWeatherService
+    {
+
+        public WeatherData.RootObject GetWeatherForecast(string city)
+        {
+            WebClient wc = new WebClient();
+            string url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&mode=json&APPID=885b91841cbfc4e0644d236d80d78621";
+            var json = wc.DownloadString(url);
+            var obj = JsonConvert.DeserializeObject<WeatherData.RootObject>(json);
+            return obj;
+        }
+    }
+}
